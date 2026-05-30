d5fea7454accf257c4e7904b547e759963bd73ca
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2026-05-30 08:12:03 +0300

    Update Game.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,19 +7,6 @@
       start a transaction
     </div>
 
-    <ul class="w-9/12 grid grid-cols-2 gap-3">
-      <li
-        :key="i"
-        v-for="(player, i) in players"
-        class="bg-gray-800 text-gray-100 p-4 rounded text-center shadow-md"
-      >
-        <div class="w-8 h-8 rounded-full bg-blue-500 inline-block" />
-        <h2 class="mt-3 font-medium text-2xl">
-          {{ player.name }}
-        </h2>
-      </li>
-    </ul>
-
     <Button class="border-red-800 text-red-800 fixed w-4/6" style="bottom: 30px" @click="endGame" variant="secondary" danger>
       End Game
     </Button>
